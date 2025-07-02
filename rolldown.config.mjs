e3ec0a0d014db26eec9bba9a4f083404bf1a5819
Alexander <github@lichter.io>
2025-07-02 14:32:58 +0200

    chore: bump `rolldown` and update benchmark results

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'rolldown';
-import path from 'path';
+import { defineConfig } from "rolldown";
+
+import path from "path";
 
 export default defineConfig({
   input: {
-    main: path.join(import.meta.dirname, 'src', 'index.js'),
+    main: path.join(import.meta.dirname, "src", "index.js"),
+  },
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  treeshake: {
+    commonjs: true,
   },
   output: {
     minify: true,
   },
+  profilerNames: false,
 });
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,1 +1,13 @@
-export default {};
+import { defineConfig } from "rolldown-vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      treeshake: {
+        commonjs: true
+      },
+      profilerNames: false
+    },
+    modulePreload: { polyfill: false }
+  },
+});
